cd03ce35704f481516ea86904c6387b5428c7eb2
Florian Kaiser <fkaiser@hs-mittweida.de>
2016-07-28 09:57:57 +0200

    fixed AtomToken index isssues added getElementBySmbol() to ElementProvider.java created rudimentary parser for atom records

--- chemistry/src/main/java/de/bioforscher/chemistry/parser/pdb/PDBToStructure.java
+++ chemistry/src/main/java/de/bioforscher/chemistry/parser/pdb/PDBToStructure.java
@@ -1,7 +1,13 @@
 package de.bioforscher.chemistry.parser.pdb;
 
+import de.bioforscher.chemistry.descriptive.elements.Element;
+import de.bioforscher.chemistry.descriptive.elements.ElementProvider;
+import de.bioforscher.chemistry.physical.Atom;
 import de.bioforscher.chemistry.physical.SubStructure;
+import de.bioforscher.mathematics.vectors.Vector3D;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -10,8 +16,42 @@ import java.util.List;
 public class PDBToStructure {
 
     public static SubStructure parseAminoAcidAtoms(List<String> lines) {
+
+        Iterator<String> iterator = lines.iterator();
+        List<SubStructure> residues = new ArrayList<>();
+        SubStructure currentResidue = null;
+        int lastResidueNumber = Integer.MIN_VALUE;
+        while (iterator.hasNext()) {
+            String currentLine = iterator.next();
+            int currentResidueNumber = Integer.valueOf(AtomToken.extractValueFromPDBLine(currentLine, AtomToken.RESIDUE_SERIAL));
+            if (currentResidueNumber > lastResidueNumber) {
+                lastResidueNumber = currentResidueNumber;
+                if (currentResidue != null) {
+                    residues.add(currentResidue);
+                    currentResidue.connectByDistance();
+                    System.out.println(currentResidue);
+                }
+                currentResidue = new SubStructure(toAtom(currentLine));
+            } else
+                currentResidue.addNode(toAtom(currentLine));
+        }
+
         return null;
     }
 
+    public static Atom toAtom(String atomLine) {
 
+        String elementSymbol = AtomToken.extractValueFromPDBLine(atomLine, AtomToken.ELEMENT_SYMBOL);
+        Element element = ElementProvider.getElementBySymbol(elementSymbol).orElseThrow(IllegalArgumentException::new);
+
+        Double x = Double.valueOf(AtomToken.extractValueFromPDBLine(atomLine, AtomToken.X_COORDINATE));
+        Double y = Double.valueOf(AtomToken.extractValueFromPDBLine(atomLine, AtomToken.Y_COORDINATE));
+        Double z = Double.valueOf(AtomToken.extractValueFromPDBLine(atomLine, AtomToken.Z_COORDINATE));
+        Vector3D coordiantes = new Vector3D(x, y, z);
+
+        Integer atomSerial = Integer.valueOf(AtomToken.extractValueFromPDBLine(atomLine, AtomToken.ATOM_SERIAL));
+        String atomName = AtomToken.extractValueFromPDBLine(atomLine, AtomToken.ATOM_NAME);
+
+        return new Atom(atomSerial, element, atomName, coordiantes);
+    }
 }
