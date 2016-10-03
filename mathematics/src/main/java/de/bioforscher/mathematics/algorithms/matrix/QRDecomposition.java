a799e662acd4b6b69f2567b67ccc0240f8247a4f
cleberecht <christoph.l@schwepnitzer.de>
2016-10-03 10:52:43 +0200

    some documentation

--- mathematics/src/main/java/de/bioforscher/mathematics/algorithms/matrix/QRDecomposition.java
+++ mathematics/src/main/java/de/bioforscher/mathematics/algorithms/matrix/QRDecomposition.java
@@ -9,26 +9,56 @@ import de.bioforscher.mathematics.vectors.VectorUtilities;
 import java.util.*;
 
 /**
- * Let A be a real Matrix with dimensions m x n (m > n) with rank(A) = n. A can be decomposed into A = Q * R, where Q
- * with dimensions m x n is orthogonal (Q^T * Q) and R is n x n and a upper triangular Matrix.
+ * Let {@code A} be a real matrix with dimensions {@code m x n (m >= n)} with {@code rank(A) = n}. {@code A} can be
+ * decomposed into {@code A = Q * R}, where {@code Q} with dimensions {@code m x n} is orthogonal ({@code Q^T * Q = E})
+ * and {@code R} is {@code n x n} and a upper triangular matrix. This so called {@code QR} decomposition or {@code QR}
+ * factorization is often used to solve the linear least squares problem, and is the basis for a particular eigenvalue
+ * algorithm, the QR algorithm.</br>
+ * <p>
+ * This class implements the modified Gram-Schmidt Algorithm, which uses the {@link
+ * VectorUtilities#orthonormalizeVectors(List)} method (see documentation there for more details). </br>
+ * <p>
+ * Use the static method {@link #calculateQRDecomposition(Matrix)} to obtain a {@link QRDecomposition} that contains the
+ * original matrix {@code A}, the orthogonal matrix {@code Q}, and the upper triangular matrix {@code R}.
  */
 public class QRDecomposition {
 
+    private Matrix originalMatrix;
+    private Matrix matrixQ;
+    private Matrix matrixR;
+
+    private QRDecomposition(Matrix originalMatrix) {
+        this.originalMatrix = originalMatrix;
+    }
+
+    public Matrix getOriginalMatrix() {
+        return this.originalMatrix;
+    }
+
+    public Matrix getMatrixQ() {
+        return this.matrixQ;
+    }
+
+    public Matrix getMatrixR() {
+        return this.matrixR;
+    }
+
     /**
-     *  The modified Gram-Schmidt Algorithm
-     * @param origin
+     * The modified Gram-Schmidt Algorithm is used to calculate the QR decomposition of the given matrix.
+     *
+     * @param originalMatrix
      */
-    public static void modifiedGramSchmidtAlgorithm(Matrix origin) {
+    public static QRDecomposition calculateQRDecomposition(Matrix originalMatrix) {
+        QRDecomposition decomposition = new QRDecomposition(originalMatrix);
         // separate matrix into column vectors
-        List<Vector> columns = MatrixUtilities.divideIntoColumns(origin);
+        List<Vector> columns = MatrixUtilities.divideIntoColumns(originalMatrix);
         // use Gram-Schmidt to calculate orthonormal vectors
         List<Vector> orthonormalizedVectors = VectorUtilities.orthonormalizeVectors(columns);
         // concatenate orthonormal columns
-        Matrix q = MatrixUtilities.matrixFromColumns(orthonormalizedVectors);
-        System.out.println("Q =\n"+q);
+        decomposition.matrixQ = MatrixUtilities.matrixFromColumns(orthonormalizedVectors);
         // compose R from previous results
-        Matrix r = composeR(columns, orthonormalizedVectors);
-        System.out.println("R =\n"+r);
+        decomposition.matrixR = composeR(columns, orthonormalizedVectors);
+        return decomposition;
     }
 
     private static Matrix composeR(List<Vector> originalColumns, List<Vector> orthonormalizedVectors) {
@@ -45,8 +75,8 @@ public class QRDecomposition {
         System.out.println("A = QR");
         System.out.println();
         Matrix a = new RegularMatrix(new double[][]{{12, -51, 4}, {6, 167, -68}, {-4, 24, -41}});
-        System.out.println("A =\n"+a);
-        modifiedGramSchmidtAlgorithm(a);
+        System.out.println("A =\n" + a);
+        calculateQRDecomposition(a);
     }
 
 
