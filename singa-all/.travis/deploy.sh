b2cfbea2cbf64a5a2f27f35cd1736b7ae09c2ca4
cleberecht <christoph.l@schwepnitzer.de>
2017-06-26 14:37:43 +0200

    trying some time out prevention script

--- singa-all/.travis/deploy.sh
+++ singa-all/.travis/deploy.sh
@@ -6,10 +6,17 @@ set -e
 # tun off so you dont have to update the version for testing travis builds
 # if [ ! -z "$TRAVIS_TAG" ]
 # then
+
     # echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
     # -DnewVersion=$TRAVIS_TAG
-    echo "deploying ..."
-    mvn -X --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -Prelease
+    mvn -X --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -Prelease &
+
+    # Output to the screen every 5 minutes to prevent a travis timeout
+    export PID=$!
+    while [[ `ps -p $PID | tail -n +2` ]]; do
+      echo 'Deploying settings'
+      sleep 300
+    done
 
     if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
         shred -v ~/.gnupg/*
@@ -18,7 +25,14 @@ set -e
 
     source .travis/gpg.sh
 
-    mvn clean deploy -X --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease
+    mvn clean deploy -X --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease &
+
+    # Output to the screen every 5 minutes to prevent a travis timeout
+    export PID=$!
+    while [[ `ps -p $PID | tail -n +2` ]]; do
+      echo 'Deploying'
+      sleep 300
+    done
 
     if [ ! -z "$TRAVIS" ]; then
         shred -v ~/.gnupg/*
