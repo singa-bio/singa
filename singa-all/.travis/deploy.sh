db21f0bf1b46a429145e57c251c9d72a9c03a5da
cleberecht <christoph.l@schwepnitzer.de>
2017-06-27 12:45:04 +0200

    updated poms and reverted travis build to tag system

--- singa-all/.travis/deploy.sh
+++ singa-all/.travis/deploy.sh
@@ -3,13 +3,11 @@
 set -e
 
 # only do deployment, when travis detects a new tag
-# tun off so you dont have to update the version for testing travis builds
-# if [ ! -z "$TRAVIS_TAG" ]
-# then
+if [ ! -z "$TRAVIS_TAG" ]
+then
 
-    # echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
-    # -DnewVersion=$TRAVIS_TAG
-    mvn -X --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -Prelease &
+    echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
+    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG -Prelease &
 
     # Output to the screen every 5 minutes to prevent a travis timeout
     export PID=$!
@@ -25,7 +23,7 @@ set -e
 
     source .travis/gpg.sh
 
-    mvn clean deploy -X --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease &
+    mvn clean deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease &
 
     # Output to the screen every 5 minutes to prevent a travis timeout
     export PID=$!
@@ -38,6 +36,6 @@ set -e
         shred -v ~/.gnupg/*
         rm -rf ~/.gnupg
     fi
-# else
-#    echo "not on a tag -> keep snapshot version in pom.xml"
-# fi
+else
+    echo "not on a tag -> keep snapshot version in pom.xml"
+fi
