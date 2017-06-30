c1b6ac02ec878395e7d3d27819c33866741fe3f8
cleberecht <christoph.l@schwepnitzer.de>
2017-06-30 09:47:23 +0200

    corrected before deploy

--- singa-all/cd/before-deploy.sh
+++ singa-all/cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_277d978f8812_key -iv $encrypted_277d978f8812_iv -in codesigning.asc.enc -out codesigning.asc -d
-    gpg --fast-import cd/signingkey.asc
+    openssl aes-256-cbc -K $encrypted_277d978f8812_key -iv $encrypted_277d978f8812_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+    gpg --fast-import cd/codesigning.asc
 fi
