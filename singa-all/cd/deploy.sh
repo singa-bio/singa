b3a86879f1fbe9f97335984e9df1e9d3f854b8b0
fkaiser <fkaiser@hs-mittweida.de>
2017-07-05 09:46:26 +0200

    updated build

--- singa-all/cd/deploy.sh
+++ singa-all/cd/deploy.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+echo "$TRAVIS_BRANCH"
+echo "$TRAVIS_PULL_REQUEST"
+echo "$TRAVIS_TAG"
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ ! -z "$TRAVIS_TAG" ];  then
     mvn deploy -P sign,build-extras --settings cd/mvnsettings.xml
 fi
