5a24ae5b17f4603e778585c704aa4f358ed3f960
Florian Kaiser <fkaiser@hs-mittweida.de>
2017-11-06 18:13:59 +0100

    removed singa-benchmark

---
+++ singa-benchmark/target/generated-sources/annotations/de/bioforscher/singa/benchmark/algorithms/superimposition/generated/Fit3DAlignmentBenchmark_jmhType.java
@@ -0,0 +1,4 @@
+package de.bioforscher.singa.benchmark.algorithms.superimposition.generated;
+public class Fit3DAlignmentBenchmark_jmhType extends Fit3DAlignmentBenchmark_jmhType_B3 {
+}
+
---
+++ singa-benchmark/target/generated-sources/annotations/de/bioforscher/singa/benchmark/mathematics/generated/MatrixBenchmark_jmhType.java
@@ -0,0 +1,4 @@
+package de.bioforscher.singa.benchmark.mathematics.generated;
+public class MatrixBenchmark_jmhType extends MatrixBenchmark_jmhType_B3 {
+}
+
