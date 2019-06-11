3b34771da89a04158687d082bc3876e3392de58f
fkaiserbio <contact@fkaiser.bio>
2019-06-11 15:38:43 +0200

    Merge branch 'develop/graph-visualizer' of /home/bioinf/s0648464/IdeaProjects/singa with conflicts.

---
+++ singa-chemistry/src/main/java/bio/singa/chemistry/features/smiles/CanonCDK.java
@@ -0,0 +1,12 @@
+package bio.singa.chemistry.features.smiles;
+
+import bio.singa.structure.model.molecules.MoleculeGraph;
+
+public class CanonCDK {
+
+    private MoleculeGraph moleculeGraph;
+
+    public CanonCDK(MoleculeGraph moleculeGraph) {
+        this.moleculeGraph = moleculeGraph;
+    }
+}
